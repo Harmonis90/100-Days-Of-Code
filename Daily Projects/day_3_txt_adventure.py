f7761a680643a411bb295e1de6eb58a8dd16afc5
Seth Kaplan <drpepperjazz@yahoo.com>
2022-02-28 18:51:19 -0500

    Daily Project 3. Text Adventure (Not Worth It ><)

---
+++ Daily Projects/day_3_txt_adventure.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+print(f'...Waking up with a hangover again... but I didn\'t drink last night.')
+isdream = input(f'Am I just drunk? \'Y\' , \'N\' ')
+if isdream.upper() == "Y":
+    print('Just another crazy night!\n I\'m going back to sleep')
+    exit()
+else:
+    print("Wait a minute... I do not remember drinking last ni... ouch. My head is killing me.")
+    print("As you look around, you are suprised to find yourself in a small room.")
+    print("There is a light shining through a door behind you.\nThere is also a closed door infront of you.")
+    choice = input("Enter 1 for going foward. Enter 2 for going back.")
+    if int(choice) == 1:
+        print("You enter a room and find a treasure chest!")
+        print("To be continued...")
+        exit()
+    elif int(choice) == 2:
+        print("You decide to go back. There\'s too much at stake.\nI'm going home")
+        exit()
+    else:
+        print("GAME OVER")
+        exit()
