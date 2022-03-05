b9fc5d03f644c19ab06cd25011606eaa125139fa
Seth Kaplan <drpepperjazz@yahoo.com>
2022-03-05 03:12:11 -0500

    Day - 8 Cipher project. Very proud of this one. Resolved a lot of unforeseen bugs and made it work. Visually and emotionally revitalizing for the soul.

--- Daily Projects/Day 8 Caesar Cipher/caesar_cipher.py
+++ Daily Projects/Day 8 Caesar Cipher/caesar_cipher.py
@@ -1,6 +1,5 @@
 import colorama as col
 import cipher_art
-import random
 import rama_func as rf
 
 alphabet_hard = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
